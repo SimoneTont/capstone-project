babfe2b39b9445dd5f6ec5cfddbab459991976b4
Simone Tonti <simone.tonti98@gmail.com>
2024-04-16 12:21:52 +0200

    admin page con tabella

--- capstone-project-react/src/pages/AdminPage.jsx
+++ capstone-project-react/src/pages/AdminPage.jsx
@@ -1,20 +1,58 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import axios from "../api/axios";
 import { Navigate } from 'react-router-dom';
 
 function AdminPage() {
-    const user = useSelector(state => state.auth.user);
     const isAdmin = useSelector(state => state.auth.user ? state.auth.user.isAdmin : false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        if (isAdmin) {
+            axios.get('http://127.0.0.1:8000/api/items')
+                .then(response => {
+                    setItems(response.data);
+                })
+                .catch(error => {
+                    console.error('Error fetching items:', error);
+                });
+        }
+    }, [isAdmin]);
 
     if (!isAdmin) {
         return <Navigate to="/" />;
-      }
+    }
+
     return (
         <div className='PageDiv'>
             <h1>Admin Page</h1>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map(item => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.description}</td>
+                            <td>{item.quantity}</td>
+                            <td>
+                                <a href={`/edit/${item.id}`} className="btn btn-warning me-2">Edit</a>
+                                <a href={`/delete/${item.id}`} className="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
+    );
 }
 
 export default AdminPage;
