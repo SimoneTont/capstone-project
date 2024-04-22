c6e1223341f38fb00e18f37796e7485d727e53a6
Simone Tonti <simone.tonti98@gmail.com>
2024-04-22 16:36:05 +0200

    css

--- capstone-project-react/src/components/FooterComponent.jsx
+++ capstone-project-react/src/components/FooterComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function FooterComponent() {
   return (
-    <footer className="SfondoArancione text-center text-lg-start OrangeText bg-dark">
+    <footer className="SfondoArancione text-center text-lg-start OrangeText bg-dark mt-5">
       <div className="text-center p-3">
         © {new Date().getFullYear()} Mu Supermarket Inc. - All Rights Reserved
       </div>
