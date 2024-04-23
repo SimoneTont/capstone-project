666feb16b4c0b362b4f6a26346e96f5e2bfc7890
Simone Tonti <simone.tonti98@gmail.com>
2024-04-23 13:17:21 +0200

    Logout e delete da mettere insieme

--- capstone-project-react/src/components/DeleteProfileButton.jsx
+++ capstone-project-react/src/components/DeleteProfileButton.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import axios from "../api/axios";
+import { Navigate } from "react-router-dom";
 
-const DeleteProfileButton = ({ userId, onDeleteSuccess }) => {
+const DeleteProfileButton = ({ userId }) => {
   const handleDelete = () => {
     if (window.confirm("Are you sure you want to delete your account?")) {
       axios
         .delete(`http://127.0.0.1:8000/api/user/delete/${userId}`)
         .then((response) => {
           console.log("User deleted successfully:", response.data);
-          onDeleteSuccess();
         })
         .catch((error) => {
           console.error("Error deleting user:", error);
-          console.log("http://127.0.0.1:8000/api/user/delete/" + userId);
           alert("Failed to delete user. Please try again.");
         });
     }
