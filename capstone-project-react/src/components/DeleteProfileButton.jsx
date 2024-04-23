bb15be1f610a8af6b925b8d92ee82e16caba074b
Simone Tonti <simone.tonti98@gmail.com>
2024-04-23 14:05:08 +0200

    delete + logout fatto

--- capstone-project-react/src/components/DeleteProfileButton.jsx
+++ capstone-project-react/src/components/DeleteProfileButton.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import axios from "../api/axios";
 import { Navigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logout } from "../redux/authSlice";
 
 const DeleteProfileButton = ({ userId }) => {
+  const [isDeleted, setIsDeleted] = React.useState(false);
+  const dispatch = useDispatch();
+
   const handleDelete = () => {
     if (window.confirm("Are you sure you want to delete your account?")) {
       axios
         .delete(`http://127.0.0.1:8000/api/user/delete/${userId}`)
         .then((response) => {
           console.log("User deleted successfully:", response.data);
+          if (response.status === 200) {
+            setIsDeleted(true);
+            dispatch(logout());
+          }
         })
         .catch((error) => {
           console.error("Error deleting user:", error);
@@ -16,6 +25,10 @@ const DeleteProfileButton = ({ userId }) => {
         });
     }
   };
+
+  if (isDeleted) {
+    return <Navigate to="/register" />;
+  }
 
   return (
     <button onClick={handleDelete} className="btn btn-danger">
